f7200790e10418f2b1ad05ae40bc85a8be1aeb0a
tomoya.otsuka@imjp.co.jp <>
2017-12-21 17:01:39 +0900

    Change .postcssrc.json to conf/.postcssrc.json

--- bash/sass.sh
+++ bash/sass.sh
@@ -4,4 +4,4 @@
 node-sass src/styles/app.scss dist/bundle.css --output-style compressed
 
 # Set prefix
-postcss dist/bundle.css -o dist/bundle.css -c .postcssrc.json
+postcss dist/bundle.css -o dist/bundle.css -c conf/.postcssrc.json
