5a5ef66a122cc4d6d939ccae8b634af0ee8e3199
tomoya.otsuka@imjp.co.jp <>
2018-01-25 19:34:16 +0900

    Add node-sass-globbing

--- bash/sass.sh
+++ bash/sass.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Compile scss to css
-node-sass src/styles/app.scss dist/bundle.css --output-style compressed
+node-sass src/styles/app.scss dist/bundle.css --output-style compressed --importer node_modules/node-sass-globbing/index.js
 
 # Set prefix
-postcss dist/bundle.css -o dist/bundle.css -c conf/postcssrc.config.json
+postcss dist/bundle.css -o dist/bundle.css -c conf/postcss.config.json
